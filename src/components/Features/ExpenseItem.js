06426bb5561683a60a83346cd073ed8692f3edb9
Muhammad-Ibrahim-Anjum <69748200+Muhammad-Ibrahim-Anjum@users.noreply.github.com>
2022-08-19 06:51:58 +0500

    react states and woring with events

--- src/components/Features/ExpenseItem.js
+++ src/components/Features/ExpenseItem.js
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./CSS/ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const updateTitle = () => {
-    setTitle("Qasim");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={updateTitle}>Change Title</button>
+      <button onClick={props.updateTitle}>Change Title</button>
     </Card>
   );
 };
